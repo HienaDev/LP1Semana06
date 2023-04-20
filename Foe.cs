5370a8d81793619a4c4bf41d440fce38ea9ac3ca
HienaDev <ant.rodrigues1999@gmail.com>
2023-04-20 14:10:45 +0100

    Added method GetName and TakeDamage

--- Foe.cs
+++ Foe.cs
@@ -19,5 +19,22 @@ namespace GameSix
             health = 100;
             shield = 100;
         }
+
+        public string GetName() => name;
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+
+            if (shield < 0)
+            {
+                
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+
+                if (health < 0 ) health = 0;
+            }
+        }
     }
 }
