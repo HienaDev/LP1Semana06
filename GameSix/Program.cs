cd73aa930959a34f4c160afac75145c04eca5701
HienaDev <ant.rodrigues1999@gmail.com>
2023-04-21 13:27:03 +0100

    Now asks for foes and its name on main

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,13 +6,27 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Foe Loki = new Foe("Loki");
+            // Asks the user how many Foes to create and save that value
+            Console.WriteLine("How many foes would you like to create?");
+            int numberOfFoes = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(Loki.GetName());
+            // Create array of foes with the size asked previously
+            Foe[] foes = new Foe[numberOfFoes];
 
-            Loki.SetName("   Grindalokki  ");
+            // Request the name of each foe
+            for (int i = 0; i < numberOfFoes; i++)
+            {
+                Console.WriteLine($"What's the name of Foe nº {i + 1}");
+                string name = (Console.ReadLine());
 
-            Console.WriteLine(Loki.GetName());
+                foes[i] = new Foe(name);
+            }
+
+            // Print name of every foe
+            foreach (Foe enemy in foes)
+            {
+                Console.WriteLine(enemy.GetName());
+            }
         }
     }
 }
